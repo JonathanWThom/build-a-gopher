7e55adaccc442276f731be6580b1c87b8c000287
Jonathan Thom <jonathan.thom1990@gmail.com>
2018-07-09 16:49:18 -0700

    Loops through array

--- main.go
+++ main.go
@@ -16,9 +16,20 @@ func main() {
 	e := Person{"Ernie", "The Dog", 8}
 	people = append(people, e)
 
-	fmt.Println(people)
+	for i, p := range people {
+		fmt.Printf("%s %s is %v years old\n", p.FirstName, p.LastName, p.Age)
+		fmt.Printf("They are number %v is this list\n\n", i)
+	}
 }
 
 // go build
 // ./build-a-gopher
-// [{Jonathan Thom 28} {Laura Syvertson 28} {Ernie The Dog 8}]
+
+// Jonathan Thom is 28 years old
+// They are number 0 is this list
+//
+// Laura Syvertson is 28 years old
+// They are number 1 is this list
+//
+// Ernie The Dog is 8 years old
+// They are number 2 is this list
