7b3616e7c815bb8676900dddb0c409a1eb567a10
Jonathan Thom <jonathan.thom1990@gmail.com>
2018-07-09 16:30:58 -0700

    Variable assignment

--- main.go
+++ main.go
@@ -3,9 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello Jonathan")
+	firstName := "Jonathan"
+	fmt.Printf("Hello %s\n", firstName)
+
+	var lastName string
+	lastName = "Thom"
+	fmt.Printf("Hello %s %s\n", firstName, lastName)
 }
 
 // go build
 // ./jonathan
 // Hello Jonathan
+// Hello Jonathan Thom
